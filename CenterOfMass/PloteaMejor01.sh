8a16c418155524b64d8390accc58a2bcc2ab382c
karel <kzapfe@gmail.com>
2015-02-12 20:04:34 -0600

    Actividad mejor determinada

--- CenterOfMass/PloteaMejor01.sh
+++ CenterOfMass/PloteaMejor01.sh
@@ -13,12 +13,13 @@ gnuplot <<EOF
 
 set xr[0:64]
 set yr[0:64]
-set cbr[-120:120]
+set cbr[-10:10]
 set size ratio -1
 
 set term pngcairo size 300,300 fontscale 0.5
 unset key
-#set palette defined (0 "#000077", 1 "#0000FF", 2 "yellow", 3 "#FF0000", 4 "#990000")
+#set palette defined (0 "#000077", 1 "#0000FF", 2 "white", 3 "#FF0000", 4 "#990000")
+set palette defined (0 "white", 1 "#0000FF", 2 "black")
 set out "$outname"
 criterio(x)=(abs(x)<60)?0:x
 plot "$dataname" matrix  w image
