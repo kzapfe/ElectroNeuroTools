4376b5d4a25095472e37d16de618ca6c5cea277b
Karel Zapfe <kzapfe@gmail.com>
2015-02-20 17:15:42 -0600

    Falta hacer el Filtro Gaussiano

--- CenterOfMass/PlotDiscLaplacian01.sh
+++ CenterOfMass/PlotDiscLaplacian01.sh
@@ -11,17 +11,16 @@ echo $outname
 
 gnuplot <<EOF
 
-set xr[0.5:63.5]
-set yr
-set cbr[-12:12]
+set xr[0.5:62.5]
+set yr [0.5:62.5]
+set cbr[-500:500]
 set size ratio -1
 
-set term pngcairo size 400,400 fontscale 0.75
+set term pngcairo size 350,350 fontscale 0.75 font "Comic Sans MS"
 unset key
 set palette defined (0 "#000077", 1 "#0000FF", 2 "white", 3 "#FF0000", 4 "#770000")
 #set palette defined (0 "white", 1 "#0000FF", 2 "black")
 set out "$outname"
-criterio(x)=(abs(x)<60)?0:x
 plot "$dataname" matrix  w image
 set out
 
