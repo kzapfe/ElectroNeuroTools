0870ed9c296b6be1f06546e598eda9a3cbd36d28
karel <kzapfe@gmail.com>
2015-04-01 12:36:04 -0600

    Funciona Disjoints

--- CenterOfMass/PloteaCMdisjoint01.sh
+++ CenterOfMass/PloteaCMdisjoint01.sh
@@ -2,17 +2,19 @@
 
 ##ploteando un chingo con varios archivos
 
-datosfondobasename="LaplacianFilteredSpaceTime-"
-datosCMbasename="CMNegLapla-"
+datosfondobasename="CSD-"
+datosCMNegname="CMNegLapla-"
+datosCMPosname="CMPosLapla-"
 numero=$1
 dataname=$datosfondobasename$numero".dat"
-datosCMNeg=$datosCMbasename$numero".dat"
+datosCMNeg=$datosCMNegname$numero".dat"
+datosCMPos=$datosCMPosname$numero".dat"
 #usandonegativo='(-$2/4.):(-$1/4):($3/500.)'
 #usandopositivo='($2/4.):($1/4):($3/500.)'
 #usandomatrix='($1/4.):($2/4):($3)'
 
-usandonegativo='($1):($2):(abs($3*0.01))'
-usandopositivo='($1):($2):($3*0.01)'
+usandonegativo='($1):($2):(abs($3*0.002))'
+usandopositivo='($1):($2):($3*0.002)'
 usandomatrix='($1):($2):($3)'
 
 outname=`basename $dataname .dat`.png
@@ -32,7 +34,7 @@ set cbr[-10:10]
 set size ratio -1
 set pointsize 0.35
 unset key
-set term pngcairo size 600,600 fontscale 0.75
+set term pngcairo size 600,600 fontscale 1.25 font "Inconsolata"
 segundos=$numero
 titulo=sprintf("Centre of Mass,  frame: g%d ", segundos)
 set title titulo
@@ -40,10 +42,11 @@ set palette defined (0 "#000077", 1 "#0000FF", 2 "white", 3 "#FF0000", 4 "#99000
 unset cbtics
 set label "Source" at graph 1.1, 0.98
 set label "Sink" at graph 1.1, 0.02
-set style fill solid noborder
+set style fill transparent solid 0.75 noborder
 #set palette defined (0 "white", 1 "#0000FF", 2 "black")
 set out "$outname"
-plot "$dataname" using $usandomatrix matrix  w image, "$datosCMNeg" usi $usandonegativo  w circles 
+#plot "$dataname" using $usandomatrix matrix  w image, "$datosCMNeg" usi $usandonegativo  w circles lw 3 lc rgb "#000075"
+plot  "$datosCMNeg" usi $usandonegativo  w circles lw 3 lc rgb "#000075", "$datosCMPos" usi $usandopositivo  w circles lw 3 lc rgb "#750000"
 set out
 
 EOF
