a79c4574e31f929e8dde67da088c319bc7595013
karel <kzapfe@gmail.com>
2015-04-16 07:45:19 -0500

    Un monton de dibujos

--- CenterOfMass/PloteaCMdisjoint01.sh
+++ CenterOfMass/PloteaCMdisjoint01.sh
@@ -29,24 +29,24 @@ echo $outname
 gnuplot <<EOF
 
 set xr[-0.50:63.5]
-set yr[-0.5:63.5]
-set cbr[-10:10]
+set yr[63.5:-0.5]
+set cbr[-120:120]
 set size ratio -1
 set pointsize 0.35
 unset key
 set term pngcairo size 600,600 fontscale 1.25 font "Inconsolata"
-segundos=$numero
-titulo=sprintf("Centre of Mass,  frame: g%d ", segundos)
+segundos=$numero/7022.
+titulo=sprintf("Centre of Mass, %3.5f s ", segundos)
 set title titulo
 set palette defined (0 "#000077", 1 "#0000FF", 2 "white", 3 "#FF0000", 4 "#990000")
 unset cbtics
 set label "Source" at graph 1.1, 0.98
 set label "Sink" at graph 1.1, 0.02
-set style fill transparent solid 0.75 noborder
+set style fill transparent solid 0.85 noborder
 #set palette defined (0 "white", 1 "#0000FF", 2 "black")
 set out "$outname"
-#plot "$dataname" using $usandomatrix matrix  w image, "$datosCMNeg" usi $usandonegativo  w circles lw 3 lc rgb "#000075"
-plot  "$datosCMNeg" usi $usandonegativo  w circles lw 3 lc rgb "#000075", "$datosCMPos" usi $usandopositivo  w circles lw 3 lc rgb "#750000"
+plot "$dataname" using $usandomatrix matrix  w image, "$datosCMNeg" usi $usandonegativo  w circles lw 3 lc rgb "#000075", "$datosCMPos" usi $usandopositivo  w circles lw 3 lc rgb "#750000"
+#plot  "$datosCMNeg" usi $usandonegativo  w circles lw 3 lc rgb "#000075", "$datosCMPos" usi $usandopositivo  w circles lw 3 lc rgb "#750000"
 set out
 
 EOF
