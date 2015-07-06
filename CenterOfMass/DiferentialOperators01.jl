5662414c91c3b1bcb1da78763764c4516ed04a72
karel <kzapfe@gmail.com>
2015-07-06 16:35:45 -0500

    Tralala

--- CenterOfMass/DiferentialOperators01.jl
+++ CenterOfMass/DiferentialOperators01.jl
@@ -1,6 +1,8 @@
 function Gradiente2D(z::Array)
+    #por cuestiones de ploteo, mejor dar dos matrices como resultado
     (alto,ancho)=size(z)
-    result=Array(Any, (alto,ancho))
+    vx=Array(Real, (alto,ancho))
+    vy=Array(Real, (alto,ancho))
     aux=copy(z)
     aux=vcat(aux[1,:],aux,aux[end,:])
     aux=hcat(aux[:,1],aux,aux[:,end])
@@ -8,27 +10,49 @@ function Gradiente2D(z::Array)
         #Derivada de la secante
         dfdx=aux[j,k+1]-aux[j,k-1]
         #los arreys tienen un eje y invertido. Su y es la -y cartesiana
-        dfdy=-(aux[j+1,k]-aux[j-1,k]) 
-        result[j-1,k-1]=(dfdx. dfdy)
+        dfdy=(aux[j+1,k]-aux[j-1,k]) 
+        (vx[j-1,k-1], vy[j-1,k-1])=(dfdx, dfdy)
     end
-    return result
+    return (vx, vy)
 end
 
         
 function Divergencia2D(z::Array)
     (alto,ancho)=size(z)
     result=Array(Real, (alto,ancho))
+    aux=copy(z)
     aux=vcat(aux[1,:],aux,aux[end,:])
     aux=hcat(aux[:,1],aux,aux[:,end])
     for j=2:alto+1, k=2:ancho+1
         #Derivada de la secante
-        vx2=aux[j,k+1]
-        vx1=aux[j,k-1]
-        vy2=aux[j+1,k]
-        vy1=aux[j-1,k] 
+        vx2=aux[j,k+1][1]
+        vx1=aux[j,k-1][1]
+        vy2=aux[j+1,k][2]
+        vy1=aux[j-1,k][2] 
         dvxdx=vx2-vx1
         #los arreys tienen un eje y invertido. Su y es la -y cartesiana
         dvydy=-(vy2-vy1)
+        result[j-1,k-1]=dvxdx+dvydy
+    end
+    return result
+end
+
+        
+function Divergencia2D(Ex::Array, Ey::Array)
+    (alto,ancho)=size(z)
+    result=Array(Real, (alto,ancho))
+    aux=copy(z)
+    aux=vcat(aux[1,:],aux,aux[end,:])
+    aux=hcat(aux[:,1],aux,aux[:,end])
+    for j=2:alto+1, k=2:ancho+1
+        #Derivada de la secante
+        vx2=Ex[j,k+1]
+        vx1=Ex[j,k-1][1]
+        vy2=Ey[j+1,k][2]
+        vy1=Ey[j-1,k][2] 
+        dvxdx=vx2-vx1
+        #los arreys tienen un eje y invertido. Su y es la -y cartesiana
+        dvydy=(vy2-vy1)
         result[j-1,k-1]=dvxdx+dvydy
     end
     return result
