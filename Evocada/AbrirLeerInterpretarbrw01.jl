be2436cdcf6e1c626444b3c2f72a705790b82188
karel <kzapfe@gmail.com>
2016-03-07 18:37:55 -0600

    Nuevo CSD y CM

---
+++ Evocada/AbrirLeerInterpretarbrw01.jl
@@ -0,0 +1,39 @@
+# Modulo para abrir los archivos BrainWave brw
+# Util para los experimentos evocados
+module AbrirLeerInterpretarbrw01
+
+using HDF5
+
+export AbreyCheca
+
+
+function AbreyCheca(x::AbstractString)
+    #Abre el archivo de brw (acepta el nombre tal cual)
+    archivo=h5open(x)
+    #sacatito todas las variables que te interesan
+    numcuadros=archivo["/3BRecInfo/3BRecVars/NRecFrames"][1][1]
+    frecuencia=archivo["/3BRecInfo/3BRecVars/SamplingRate"][1][1]
+    maxvolt=archivo["/3BRecInfo/3BRecVars/MaxVolt"][1][1]
+    minvolt=archivo["/3BRecInfo/3BRecVars/MinVolt"][1][1]
+    bitdepth=archivo["/3BRecInfo/3BRecVars/BitDepth"][1][1]
+    duracionexperimento=numcuadros/frecuencia
+    factordeescala=(maxvolt-minvolt)/2^bitdepth
+    DatosCrudos=read(archivo["/3BData/Raw"])
+    result=Dict("numcuadros" => numcuadros,
+                "frecuencuia"=> frecuencia,
+                "maxvolt" => maxvolt,
+                "minvolt" => minvolt,
+                "bitdepth" => bitdepth,
+                "duracion" => duracionexperimento,
+                "factor " => factordeeescala,
+                "DatosCrudos"=>DatosCrudos )
+    return result
+                    
+end
+
+    
+
+    
+
+
+end
