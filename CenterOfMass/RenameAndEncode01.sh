a8ef1bb0abc4cd0b6479b2c53779c978a430f19f
Karel Zapfe <kzapfe@gmail.com>
2016-03-15 15:26:39 -0600

    Ya con CSD el evocado

--- CenterOfMass/RenameAndEncode01.sh
+++ CenterOfMass/RenameAndEncode01.sh
@@ -23,6 +23,10 @@ rename $argumento $argumento$tresceros $argumento$unnum".png"
 
 echo $paraencoderbien
 
-mencoder "$paraencoderbien"  -o $outputbien -ovc lavc -mf fps=12
+##Consejos de Mario Valle para mejor imagenes cientificas hard-edges
+opt="vbitrate=2160000:mbd=2:keyint=132:v4mv:vqmin=3:lumi_mask=0.07:dark_mask=0.2:mpeg_quant:scplx_mask=0.1:tcplx_mask=0.1:naq"
+
+mencoder "$paraencoderbien"  -o $outputbien -ovc lavc -mf fps=6 -lavcopts $opt
+
 
 
