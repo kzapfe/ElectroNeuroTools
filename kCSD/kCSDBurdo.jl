d4823cde403c557373cf646759edc7e6e2e5e14a
Karel Zapfe <kzapfe@gmail.com>
2016-12-02 14:51:00 -0600

    kCSD Burdo listo

---
+++ kCSD/kCSDBurdo.jl
@@ -0,0 +1,51 @@
+using JLD
+
+numarg=length(ARGS)
+if numarg<1
+    error("Dame un nombre de archivo JLD que tenga LFP guardados")
+else
+    nombre=ARGS[1] 
+end
+
+println("cargando lfp")
+lfp=load(nombre)["LFPSaturados"]
+
+tmax=size(lfp,3)
+
+if isfile("KTT_Suave_r_1.0_4095.dat")
+    KTT=readdlm("KTT_Suave_r_1.0_4095.dat")
+    println("KTT leida del disco")
+else
+    K=readdlm("K_Suave_r-1.0-4095.dat")
+    KTilde=readdlm("KTilde_Suave_r-1.0-4095.dat")
+    Kinv=inv(K)
+    KTT=transpose(KTilde)*Kinv;
+    writedllm("KTT_Suave_r_1.0_4095.dat", KTT)
+    println("KTT Kinv calculada y guardada")
+end
+
+
+
+
+CSDtentativa=zeros(4096)
+CSD=zeros(lfp)
+
+println("Empezando calculo")
+for t=1:tmax
+    LFPvector1=reshape(transpose(lfp[:,:,t]),4096);
+    lfpvecredux=LFPvector1[2:end]
+    CSDtentativa[2:end]=transpose(KTT)*lfpvecredux
+    CSD[:,:,t]=reshape(CSDtentativa,64,64)
+end
+
+println("terminando calculo")
+
+paguardar=load(nombre)
+paguardar["kCSD"]=CSD
+save(nombre,paguardar)
+
+println("Tu archivo jld ha sido modificado, checa una entrada kCSD")
+
+
+
+
