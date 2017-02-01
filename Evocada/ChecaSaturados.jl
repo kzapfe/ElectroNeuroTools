da0fd981d0684d71441447fe9bd99cd1242d1e8c
Karel Zapfe <kzapfe@gmail.com>
2017-02-01 13:49:16 -0600

    Mas kCSDA

--- Evocada/ChecaSaturados.jl
+++ Evocada/ChecaSaturados.jl
@@ -1,11 +1,15 @@
-using JLD
+using JLD,HDF5
 
 listacompleta=readdir()
 
 for i in listacompleta    
-    if (i[end-2:end]=="jld")
-        saturados=load(i)["CanalesSaturados"]
-        tantos=length(saturados)
-        println(i, " ", tantos)
+    if (i[end-2:end]=="jld" && i[1:3]=="LFP")
+        try
+            saturados=load(i)["CanalesSaturados"]
+            tantos=length(saturados)
+            println(i, " ", tantos)
+        catch
+            println(i, " esto no tiene lo que buscas")
+        end
     end
 end
