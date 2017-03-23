ae92e5eefcea7bba8eb40ef3f2423f35407f454a
Karel Zapfe <kzapfe@gmail.com>
2017-03-23 14:51:56 -0600

    pequenos ajustes al mapeo de trazos

--- Graficando/ploteaunchingo.jl
+++ Graficando/ploteaunchingo.jl
@@ -1,10 +1,12 @@
 
+
 using JLD,PyPlot
 
-nombre="../JLDFiles/LFPOriginales/Deshinibido/evento_desinhibido_3.jld"
+nombre="/home/karel/RGutWork/JLDFiles/evento_deshinibido_2.jld"
 #nombre=ARGS[1]
 arx=load(nombre)
 lfp=arx["LFPSaturados"]
+csd=arx["kCSDCorrecta"]
 (alto,ancho,tmax)=size(lfp)
 
 maxabs=zeros(alto,ancho)
@@ -18,22 +20,22 @@ maxmax=maximum(maxabs)
 maxabs/=maxmax
 
 xxs1=[600,1600]
-yys1=[-100,-100]
+yys1=[400,400]
 xxs2=[601,601]
 yys2=[-100,400]
 
 
 ion()
-imagen=figure(figsize=(16,16))
+imagen=figure(figsize=(15,16))
 axis("off")
 xlim(100,1600+16000)
-ylim(-620,9000)
+ylim(9000,-620)
 plot(xxs1,yys1, lw=2,c="black")
-plot(xxs2,yys2, lw=2,c="black")
-text(650,-70,"142ms")
-text(650,100, "0.5mV")
+#plot(xxs2,yys2, lw=2,c="black")
+text(650,270,"142ms")
+#text(650,20, "0.5mV")
 for j=1:4:64 ,k=1:4:64
-    plot((600:1600)+(k-1)*265,lfp[j,k,600:1600]+(j-1)*125,
+    plot((600:1600)+(k-1)*265,-vec(csd[j,k,600:1600])+(j-1)*125,
          c="black",lw=0.1)
         # edgecolors=float(j),cmap="jet")
 end
@@ -41,9 +43,7 @@ for j=1:4:64
     text((j+1)*265,-300, "$j")
     text(200,(j-1)*125, "$j")
 end
-#savefig("MapaTrazosCSDDeshinibida03.svg",dpi=90)
-#savefig("MapaTrazosCSDDeshinibida03.png",dpi=90)
-
-
+savefig("MapaTrazosCSDDeshinibida02.svg",dpi=90)
+savefig("MapaTrazosCSDDeshinibida02.png",dpi=90)
 close()
 close()
