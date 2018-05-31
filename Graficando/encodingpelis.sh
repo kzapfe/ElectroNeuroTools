b1f35faaac19bb2ba650219dae7a017219d46d11
karel <kzapfe@gmail.com>
2018-05-31 14:02:57 -0500

    lala

--- Graficando/encodingpelis.sh
+++ Graficando/encodingpelis.sh
@@ -2,4 +2,4 @@
 
 nomigeneral=$1
 
-mencoder "mf://$nomigeneral*png" -mf fps=6 -o prueba.avi -ovc lavc -lavcopts vcodec=mpeg4:vbitrate=800^C
+mencoder "mf://$nomigeneral*png" -mf fps=6 -o prueba.avi -ovc lavc -lavcopts vcodec=mpeg4:vbitrate=800
