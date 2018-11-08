023fa26d339e91f68e0e3fe22ec9ec2cd4efaa22
karel <kzapfe@gmail.com>
2018-11-08 17:30:27 -0600

    nuevo Otsu

---
+++ Preprocesamiento/Otsu.jl
@@ -0,0 +1,51 @@
+module Otsu
+
+using Statistics
+
+export stdventana, otsuventana, otsumethod
+
+function stdventana(datos::Array, ancho::Int)
+    # de un array lineal, sacar la desviacion
+    # estandar por ventanas de ancho dado
+    anchomedio=floor(ancho/2)
+    tantos=convert(Int, floor((length(datos)-ancho)/paso))
+    result=zeros(tantos)
+    for t=1:tantos
+        result[t]=std(datos[(t-1)*paso+1:(t-1)*paso+ancho])
+    end
+    return result
+end
+
+function otsumethod(datos::Array)
+    #=
+    Metodo de Otsu para separar una imagen en dos subconjuntos.
+    =#
+    minimo=minimum(datos)
+    maximo=maximum(datos)
+    binsdefault=2*ceil(Int,sqrt(length(datos)))
+    h=fit(Histogram, vec(datos), nbins=binsdefault)
+    (rango, cuentas)=(h.edges, h.wheights)
+    tantos=length(rango)
+    # valores
+    omega1=0
+    omega2=0
+    mu1=0
+    mu2=0
+    sigmab=0
+    sigmabtemp=0
+    tbest=0
+    varlim=0
+    for t=1:tantos-1
+        omega1=sum(cuentas[1:t])
+        omega2=sum(cuentas[t+1:tantos-1])
+        mu1=sum(cuentas[1:t].*rango[1:t])/omega1
+        mu2=sum(cuentas[t+1:tantos-1].*rango[t+1:tantos-1])/omega2        
+        sigmabtemp=omega1*omega2*((mu1-mu2)^2)
+        if sigmabtemp>sigmab
+            sigmab=sigmabtemp
+            tbest=t
+            varlim=rango[t]
+        end
+    end
+    return (sigmab,tbest,varlim)
+end
