5ecc292255a5bf2012dad91ffffe1bdde6ac8f2a
Karel Zapfe <kzapfe@gmail.com>
2019-06-25 16:03:03 -0500

    plotalot

--- Graficando/ploteaunchingocsd.jl
+++ Graficando/ploteaunchingocsd.jl
@@ -1,47 +1,84 @@
 using HDF5
 using PyPlot
+using Statistics
 
 
 nombre=ARGS[1]
 
 arxivo=h5open(nombre)
 elementos=names(arxivo)
-
 csdlind=read(arxivo["CSDALindenberg"])
+
+sigma=std(csdlind)
+mu=mean(csdlind)
+
+function subruido(x,u)
+    result=0
+    if -u <x < u
+        result=0
+     else
+        result=x
+    end
+    return result
+ end
+
 (alto,ancho,nmax)=size(csdlind)
+intervalo=1:5:nmax
 #(xmin, xmax)=read(arxivo["cols"])
 #(ymin, ymax)=read(arxivo["rengs"])
 freq=read(arxivo["freq"])
 
-(mincsd, maxcsd)=extrema(csdlind)
-limites=min(abs(mincsd),abs(maxcsd))/1.5
 
+if length(ARGS) == 1
+    nomout="csdplot_"
+else
+    nomout=ARGS[2]
+end
+
+
+ini=floor(Int, 400*freq)
+fin=floor(Int, 500*freq)
+
+
+(mincsd, maxcsd)=extrema(csdlind)
+#limites=min(abs(mincsd),abs(maxcsd))/1.25
+limites=5*sigma
+
+close(arxivo)
+
+println(limites, " es la marca de color csd maximo/minimo")
+
+csdadulterada=map(x->subruido(x,2.5*sigma), csdlind) 
+
+
+csdlind=0
 #for interpol in lista
 
 
-for n =1:nmax
+#for n =1:5:nmax
+for n=ini:fin
 
     xlim(0,ancho+1)
     ylim(0, alto+1)
     
     fafa=figure(figsize=(4,4))
-    ejemplo=csdlind[:,:,n]
+    ejemplo=csdadulterada[:,:,n]
     tiempo=round(Int,n/freq)
 
     title("t = $tiempo ms")
     
    
     imshow(ejemplo, origin="lower",
-           cmap="coolwarm", interpolation="nearest",
+           cmap="coolwarm", interpolation="bicubic",
            vmin=-limites, vmax=limites,extent=[0.5,ancho+0.5,0.5,alto+0.5])
 
     nstring=lpad(n, 8, "0")
     
-    nomine="csd_eduardo_193005_control_01_$nstring.png"
+    nomine=nomout*"$nstring.png"
 
     savefig(nomine, dpi=92)
     close(fafa)
-    if mod(n,10)==0
+    if mod(n-1,50)==0
         print(" n = ", n, "; ")
     end
 end
